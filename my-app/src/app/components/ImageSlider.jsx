1c78ffddc234681c34255219c86462c8c8adb881
devikasharma0 <89690020+devikasharma0@users.noreply.github.com>
2023-06-13 00:14:36 +0530

    Image slider and super creative button

---
+++ my-app/src/app/components/ImageSlider.jsx
@@ -0,0 +1,32 @@
+import Image from 'next/image'
+import React from 'react'
+import{TfiArrowCircleLeft, TfiArrowCircleRight} from 'react-icons/tfi'
+import{MdOutlineDoubleArrow} from 'react-icons/md'
+
+function ImageSlider() {
+  return (
+    <>
+    <div className='grid place-items-center'>
+        <Image src = "/Images/ecom.png" alt='ecom' width={1400} height={600} className='border-2 border-gray-800'/>
+        <div className='absolute top-1/2 left-8 rounded-full bg-slate-200 p-2'>
+            <TfiArrowCircleLeft/>
+        </div>
+        <div className='absolute top-1/2 right-8 rounded-full bg-slate-200 p-2'>
+            <TfiArrowCircleRight/>
+        </div>
+
+    </div>
+    <div className='flex justify-between mx-48 my-20 bg- py-7 px-10 rounded-md bg-[#0E2954]'>
+        <div>
+            <h1 className='text-3xl text-white '>Deals of the day</h1>
+            <p className='text-sm text-white'>Check out deals on top brands!</p>
+        </div>
+        <div className='hover:bg-white w-36 border border-white flex items-center justify-start text-white hover:text-[#0E2954] '>
+            <button className='text-5xl transition duration-500 hover:translate-x-20'><MdOutlineDoubleArrow/></button>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default ImageSlider
--- my-app/src/app/page.jsx
+++ my-app/src/app/page.jsx
@@ -1,12 +1,16 @@
 import Image from 'next/image'
 import NavBar from './components/NavBar'
 import NavBar2 from './components/NavBar2'
+import Categories from './components/Categories'
+import ImageSlider from './components/ImageSlider'
 
 export default function Home() {
   return (
 <>
 <NavBar/>
 <NavBar2/>
+<Categories/>
+<ImageSlider/>
 </>
   )
 }
